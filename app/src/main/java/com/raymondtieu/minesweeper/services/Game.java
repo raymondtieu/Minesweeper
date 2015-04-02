ec79ca605ef1aedd4bf2aa3cbe1cf68ee9fa8593
Raymond <iamraymondtieu@gmail.com>
2015-04-02 17:16:46 -0400

    game code indentation

--- app/src/main/java/com/raymondtieu/minesweeper/services/Game.java
+++ app/src/main/java/com/raymondtieu/minesweeper/services/Game.java
@@ -4,10 +4,10 @@ package com.raymondtieu.minesweeper.services;
  * Created by raymond on 2015-04-02.
  */
 public interface Game {
-	public void startGame(int x, int y);
-	public boolean revealCell(int x, int y);
-	public void revealAdjacent(int x, int y);
-	public void markCell(int x, int y);
-	public void markAdjacent(int x, int y);
-	public boolean gameOver();
+    public void startGame(int x, int y);
+    public boolean revealCell(int x, int y);
+    public void revealAdjacent(int x, int y);
+    public void markCell(int x, int y);
+    public void markAdjacent(int x, int y);
+    public boolean gameOver();
 }
