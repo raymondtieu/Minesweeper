28deeed0bddd11f96509c902edb872324452ebcf
Raymond <iamraymondtieu@gmail.com>
2015-04-03 22:33:50 -0400

    menu inflater with difficulties

--- app/src/main/java/com/raymondtieu/minesweeper/activities/MainActivity.java
+++ app/src/main/java/com/raymondtieu/minesweeper/activities/MainActivity.java
@@ -1,5 +1,6 @@
 package com.raymondtieu.minesweeper.activities;
 
+import android.content.Intent;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -10,6 +11,8 @@ import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.raymondtieu.minesweeper.fragments.*;
 
@@ -19,21 +22,14 @@ public class MainActivity extends ActionBarActivity {
 
     private Toolbar toolbar;
 
+    private Button easyMode, mediumMode, hardMode;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toolbar = (Toolbar) findViewById(R.id.app_bar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-
-        NavigationDrawerFragment drawerFragment;
-        drawerFragment = (NavigationDrawerFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.fragment_navigation_drawer);
-
-        drawerFragment.setUp(R.id.fragment_navigation_drawer,
-                (DrawerLayout) findViewById(R.id.drawer_layout), toolbar);
+        setUpToolbar();
     }
 
     @Override
@@ -51,10 +47,28 @@ public class MainActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_easy) {
+            return true;
+        } else if (id == R.id.action_medium) {
+            return true;
+        } else if (id == R.id.action_hard) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void setUpToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        NavigationDrawerFragment drawerFragment;
+        drawerFragment = (NavigationDrawerFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.fragment_navigation_drawer);
+
+        drawerFragment.setUp(R.id.fragment_navigation_drawer,
+                (DrawerLayout) findViewById(R.id.drawer_layout), toolbar);
+    }
+
 }
