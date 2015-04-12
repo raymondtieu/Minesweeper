c741ab8c2a6632b43d42e948ebec1fe434fce02e
Raymond <iamraymondtieu@gmail.com>
2015-04-12 02:59:34 -0400

    Cell model

---
+++ app/src/main/java/com/raymondtieu/minesweeper/models/Cell.java
@@ -0,0 +1,36 @@
+package com.raymondtieu.minesweeper.models;
+
+/**
+ * Created by raymond on 2015-04-12.
+ */
+public class Cell {
+
+    public enum Status {
+        HIDDEN, REVEALED, FLAGGED, FLAG_CORRECT, FLAG_INCORRECT
+    }
+
+    private int adjacentMines;
+    private Status status;
+
+    public Cell() {
+        adjacentMines = 0;
+        status = Status.HIDDEN;
+    }
+
+
+    public int getAdjacentMines() {
+        return adjacentMines;
+    }
+
+    public void setAdjacentMines(int adjacentMines) {
+        this.adjacentMines = adjacentMines;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+}
